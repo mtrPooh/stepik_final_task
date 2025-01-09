a297d8c8597df618ba2d8c50a0eaf40beabc6ef7
mtrPooh <it-exxpert@yandex.ru>
2025-01-09 21:23:51 +0500

    Added checking for  element is present method and test

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,12 @@
-from selenium.webdriver.common.by import By
+import pytest
 from .pages.main_page import MainPage
 import time
 
-def test_guest_can_go_to_login_page(browser):
+def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    page.should_be_login_link()
     time.sleep(10)
 
